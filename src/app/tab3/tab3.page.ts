88ff9aec4ead2f8fc6b33c0592979b7dfee3dc9d
Christian Smith <cgr.gonzalezrossier@gmail.com>
2019-05-12 01:59:04 -0400

    Detalles esteticos finales

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -8,14 +9,42 @@ import { Router } from '@angular/router';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
   constructor(
     private auth: AuthService,
-    private router: Router
-    ) {}
+    private router: Router,
+    public alertCtrl: AlertController
+  ) {}
+
   cerrarSesion() {
     this.auth.signOut().then(() => {
       this.router.navigate(['login']);
     });
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertCtrl.create({
+      header: '¿Estás seguro?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {}
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.cerrarSesion();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  segmentChanged(event: any) {
+    const valorSegment = event.detail.value;
+    console.log(valorSegment);
   }
 }
